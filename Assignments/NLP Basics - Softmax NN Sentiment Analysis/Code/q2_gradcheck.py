a6b3b70fa7db883b4ec45720ae24a95e00ef3fa5
mustafa-syed <muhammad.hasan@gmail.com>
2017-06-14 01:10:37 +0500

    removed comment

--- Assignments/NLP Basics - Softmax NN Sentiment Analysis/Code/q2_gradcheck.py
+++ Assignments/NLP Basics - Softmax NN Sentiment Analysis/Code/q2_gradcheck.py
@@ -30,7 +30,6 @@ def gradcheck_naive(f, x):
         # to test cost functions with built in randomness later.
 
         ### YOUR CODE HERE:
-        # what to write here... I need to study a little before implementation
         raise NotImplementedError
         ### END YOUR CODE
 
